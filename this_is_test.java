9c59bf7096c7ba0de25c70716575dd7e52aaa9bd
Amal benny <57700315+Amalbenny619@users.noreply.github.com>
2023-09-29 16:51:16 +0530

    Create this_is_test.java
    This just a sample code to test please dont debug , wont work

---
+++ this_is_test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public TESTNEWJAVACODE {
+
+   public static feelingSleepy(){
+     System.out.println("if you are feeling sleepy call me out ")
+       Thread.sleep()
+   }
+   public static void main(String [] args){
+          System.out.println("hi there does it works+") 
+
+            for(int i =0; i<n; i++){
+            System.out.println("not sure why im writing these")
+
+              feelingSleepy()
+            }
+     
+   }
+
+  
+  
+}
+
